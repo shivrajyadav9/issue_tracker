7cd890333438b5a144130eb0803cdf8b609cc369
Shivraj <rajs777yadav@gmail.com>
2023-12-09 22:42:10 +0530

    commit

--- config/environment.js
+++ config/environment.js
@@ -9,7 +9,16 @@ const development = {
     name: 'development',
     asset_path: './assets',
     session_cookie_key: 'blahsomething',
-    db: 'issue_tracker_development'
+    db: 'issue_tracker_development',
+    mongodb_uri: 'mongodb://0.0.0.0:27017/issue_tracker_development'
 }
 
-export default development;
+const production = {
+    name: 'production',
+    asset_path: './assets',
+    session_cookie_key: 'blahsomething',
+    db: 'issue_tracker_development',
+    mongodb_uri: 'mongodb+srv://vercel-admin-user-65749921cfd0db7119502f37:zMeYyDKYK7lxtf0p@cluster0.fk9lx9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
+export default eval(process.env.MONGODB_URI == undefined ? development : production);
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,7 @@
 import mongoose from 'mongoose';
 import env from './environment.js';
 
-// mongoose.connect(`mongodb://0.0.0.0:27017/${env.db}`);
-//mongodb + srv://vercel-admin-user-65749921cfd0db7119502f37:zMeYyDKYK7lxtf0p@cluster0.fk9lx9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
-// mongoose.connect("mongodb+srv://vercel-admin-user-65749921cfd0db7119502f37:zMeYyDKYK7lxtf0p@cluster0.fk9lx9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(env.mongodb_uri);
 
 const db = mongoose.connection;
 
