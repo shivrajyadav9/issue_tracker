9afb06f1c9a9201ab7c649180c795427a64fc98a
Shivraj <rajs777yadav@gmail.com>
2023-12-11 12:52:54 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import env from './environment.js';
 
-mongoose.connect('mongodb+srv://shivraj:mongodbcluster@cluster0.fk9lx9t.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://shivraj:mongodbcluster@cluster0.fk9lx9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 // mongoose.connect('mongodb+srv://vercel-admin-user-65749921cfd0db7119502f37:zMeYyDKYK7lxtf0p@cluster0.fk9lx9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&authSource=admin');
 
 const db = mongoose.connection;
